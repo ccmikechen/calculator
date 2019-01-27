e4af82bb665658e137e0559a8341fb21641d4608
ccmikechen <ccmikechen@gmail.com>
2019-01-27 20:45:51 +0800

    Initial commit

---
+++ 計算/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(295, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(102, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "答案 ： "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(157, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 85)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "計算機"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ 計算/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+
+    Function CP(ByVal S As String) As Double
+        S = Replace(S, " ", "")
+        Dim t As String = ""
+        Dim f As Integer
+        For Each i In S '
+            Select Case i '分割最外層括號 x-(y+z)*2(4-2) → x- (y+z) *2* (4-2)
+                Case "("
+                    If t <> "" And f = 0 Then
+                        Select Case Microsoft.VisualBasic.Right(t, 1)
+                            Case 0 To 9, " " : t &= "*" 'x(y) > x*(y)
+                        End Select
+                        t &= " "
+                    End If
+                    f += 1
+                    t &= i
+                Case ")"
+                    f -= 1
+                    If f = 0 Then t &= ") " Else t &= i
+                Case Else
+                    t &= i
+            End Select
+        Next
+        Dim M() As String = Split(t, " ")
+        For i = 0 To UBound(M) '計算括號內的值，傳回值覆蓋式子
+            If InStr(M(i), "(") > 0 Then M(i) = CP(Mid(M(i), 2, Len(M(i)) - 2))
+        Next
+
+        Dim A As String = Join(M.ToArray, "") '合併括號計算後的值
+        A = Replace(A, "--", "+") '負負得正
+        Dim M2() As String = Split(A, "+")
+        Dim ans As Double = 0
+        For Each i In M2
+            ans += Subtract(i) '加法運算(優先度最低)
+        Next
+
+        Return ans
+    End Function
+
+    Function Subtract(ByVal S As String) As Double
+        S = Replace(S, "-", " -")
+        S = Replace(S, "* -", "*-")
+        S = Replace(S, "/ -", "/-")
+        S = Replace(S, "^ -", "^-")
+        Dim M() As String = Split(S, " ")
+        Dim ans As Double = Multiply(M(0))
+        For i = 1 To UBound(M)
+            ans += Multiply(M(i)) '減法運算
+        Next
+        Return ans
+    End Function
+
+    Function Multiply(ByVal S As String) As Double
+        Dim M() As String = Split(S, "*")
+        Dim ans As Double = 1
+        For Each i In M
+            ans *= Divide(i) '乘法運算
+        Next
+        Return ans
+    End Function
+
+    Function Divide(ByVal S As String) As Double
+        Dim M() As String = Split(S, "/")
+        Dim ans As Double = Power(M(0))
+        For i = 1 To UBound(M)
+            ans /= Power(M(i)) '除法運算
+        Next
+        Return ans
+    End Function
+
+    Function Power(ByVal S As String) As Double
+        Dim M() As String = Split(S, "^")
+        Dim ans As Double = Val(M(UBound(M)))
+        For i = UBound(M) - 1 To 0 Step -1
+            ans = Val(M(i)) ^ ans '冪運算(優先度最高)
+        Next
+        Return ans
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim st As String = TextBox1.Text
+        Label2.Text = CP(st)
+    End Sub
+End Class
